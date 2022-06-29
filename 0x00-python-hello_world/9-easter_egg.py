e20d63e6f65635c778f7218b9138a5796c344a9f
abiolaks <kingsleylawani@gmail.com>
2022-06-29 14:37:28 -0700

    zen of python

---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+print("The Zen of Python, by Tim Peters")
+print()
+print()
+print("Beautiful is better than ugly.\nExplicit is better than implicit.\n\
+Simple is better than complex.\nComplex is better than complicated.\n\
+Flat is better than nested.\nSparse is better than dense.\nReadability counts.\n\
+Special cases aren't special enough to break the rules.\nAlthough practicality beats purity.\nErrors should never pass silently.\nUnless explicitly silenced.\n\
+In the face of ambiquity, refuse the temptation to guess.\n\
+There should be one-- and preferably only one --obvious way to do it.\n\
+Although that way may not be obvious at first unless you're a Dutch.\n\
+Now is better than never.\nAlthough never is often better than *right now.\n\
+If the implementation is hard to explain, it's a bad idea.\n\
+If the implementation is easy to explain, it  may be a good idea.\n\
+Namespaces are one honking great idea -- let's do more of those!.");
