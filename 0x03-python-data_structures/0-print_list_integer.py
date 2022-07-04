d7a33656e9ddd9c27ce79e7c2c62b26bbd657751
abiolaks <kingsleylawani@gmail.com>
2022-07-04 05:23:48 -0700

    print list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,10 +1,12 @@
-# defining a function to print an integer in a list
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
     """
+
     A function to print integer per line
+
     """
+
     my_len = len(my_list)
 
     for i in range(my_len):
