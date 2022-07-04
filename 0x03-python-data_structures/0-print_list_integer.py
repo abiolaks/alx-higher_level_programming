c74cf8ae6ea50c049ee5909f9937d0a800d6cf57
abiolaks <kingsleylawani@gmail.com>
2022-07-04 05:22:33 -0700

    print list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -5,5 +5,7 @@ def print_list_integer(my_list=[]):
     """
     A function to print integer per line
     """
-    for i in my_list:
+    my_len = len(my_list)
+
+    for i in range(my_len):
         print("{:d}".format(my_list[i]))
