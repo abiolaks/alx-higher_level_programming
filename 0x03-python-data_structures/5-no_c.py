714c801fc65265698c2054294ee6a06a9042ef85
abiolaks <kingsleylawani@gmail.com>
2022-07-04 16:18:13 -0700

    skip cs

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    """
+    function tha removes all character c and C form a string
+    """
+    if my_string:
+        new_string = ""
+
+        for char in my_string:
+            if char == 'c' and char == 'C'
+                continue
+            new_string = new_string + char
+        return new_string
