9272f3571f1ff93ce885ad80986ec6e822f5eed2
abiolaks <kingsleylawani@gmail.com>
2022-07-04 05:55:22 -0700

    getting value in an index of a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,8 +3,8 @@
 def element_at(my_list, idx):
     """
     this function retrieves an element from a list
-    it returns None if the search index is -ve or out of bound
     """
+
     list_len = len(my_list)
 
     if idx < 0:
