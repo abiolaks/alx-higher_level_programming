36663a03ec5a1cf625a4e0e2d0f318c34088dc6f
abiolaks <kingsleylawani@gmail.com>
2022-07-04 16:20:11 -0700

    skip cs

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -9,6 +9,6 @@ def no_c(my_string):
 
         for char in my_string:
             if char == 'c' and char == 'C'
-                continue
+                c = c + 1
             new_string = new_string + char
-    return new_string
+        return new_string
