525b72b818b18fe3737b447654c3b6096138c15b
abiolaks <kingsleylawani@gmail.com>
2022-07-04 18:23:25 -0700

    matrx

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,10 @@ def print_matrix_integer(matrix=[[]]):
     write a function that prints a matriz of integers
     """
 
-    for each_list in matrix:
-        for row in each_list:
-            print("{:d}".format(row), end=" ")
-        print()
+#    for each_list in matrix:
+#        for row in each_list:
+#            print("{:d}".format(row), end=" ")
+ #       print()
+     for elm in matrix:
+        print(" ".join("{:d}".format(i) for i in elm))
+
